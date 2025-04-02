8f4a85108b8eba77d9c40441ad89c465de794ad9
ForSureN1 <standin1532@gmail.com>
2025-04-02 12:45:31 +0500

    1

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="img/icon.svg" type="image/x-icon">
     <title>{{ __('menu.main') }}</title>
     <!--
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.css?_v=20250320171057"> -->
